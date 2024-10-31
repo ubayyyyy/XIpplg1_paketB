21ad4c650a1c937acd2553b8f2936e6dd0f847d3
ubayyyyy <ubaidillah.asy4@smk.belajar.id>
2024-10-31 09:42:33 +0700

    ubay

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="NavbarNavAltMarkup">
+      <ul class="navbar-nav me-auto">
+        <a href="register.php" class="btn btn-outline-primary m-1">
+            Daftar
+        </a>
+        <a href="login.php" class="btn btn-outline-success m-1">
+            Masuk
+        </a>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" title="" style="height:12rem;">
+                <div class="car-footer text-center">
+                    <a href="">10 Suka</a>
+                    <a href="">3 Komentar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+    <p>&copy; UJIKOM RPL 2024 | Ubaidillah Asy-Syakir & Bayu Saputra</p>
+</footer>
+
+<script type="text/javascript" src="asset/js/boostrap.min.js"></script>
+</body>
+</html>
