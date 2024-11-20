ef2611eb5abb68e3438845dddd52bcc1016c9748
ubayyyyy <ubaidillah.asy4@smk.belajar.id>
2024-11-20 19:36:37 +0700

    bayu

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" title="" style="height: 12rem;">
+                <div class="card-footer text-center">
+                    <a href="">10 suka</a>
+                    <a href="">3 komentar</a>
+                </div> 
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="d-flex justify-content-center border-top mt-3bg-light fixed bottom">
+
+</footer>
+
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
