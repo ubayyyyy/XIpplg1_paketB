710b3060863fad1553ae8bfe5783a1e2c4114ca4
ubayyyyy <ubaidillah.asy4@smk.belajar.id>
2024-11-20 19:59:09 +0700

    bayu

---
+++ user/index.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+$userid = $_SESSION['userid'];
+include '../config/koneksi.php';
+
+if ($_SESSION['status'] != 'login') {
+  echo "<script>
+    alert('Anda belum Login!');
+    location.href='../index.php';
+    </script>";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Website Galeri Foto</title>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+        <div class="navbar-nav me-auto">
+          <a href="home.php" class="nav-link">Home</a>
+          <a href="album.php" class="nav-link">Album</a>
+          <a href="foto.php" class="nav-link">Foto</a>
+        </div>
+        <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
+      </div>
+    </div>
+  </nav>
+
+  
+        </div>
+      <?php  ?>
+    </div>
+  </div>
+
+  </div>
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
