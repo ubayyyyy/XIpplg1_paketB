684ffb0fc3c015405acb5b059b3dcf900184a260
ubayyyyy <ubaidillah.asy4@smk.belajar.id>
2024-11-20 19:34:08 +0700

    Delete halaman.php

--- halaman.php
+++
@@ -1,5 +0,0 @@
-<?php session_start(); include "koneksi.php"; ?>
-HALO SELAMAT DATANG!! <br/>
-Anda Login Sebagai <strong><?php echo  $_SESSION['level']; ?></strong>
-<br/><br/>
-<a href="login.php">BACK</a>
